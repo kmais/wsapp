fe87c9008e91bd04a4c30ceeebb119a39df87cf1
Kenny <kennymais@gmail.com>
2019-12-26 09:55:57 -0600

    lil 2a app

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ app.get("/", function(req, res) {
 io.sockets.on("connection", function(socket) {
   connections.push(socket);
   console.log("connected: %s sockets connected", connections.length);
-});
 
-io.socket.on("disconnect", function(data) {
-  //disconnect
-  connections.splice(connections.indexOf(socket), 1);
-  console.log("disconnected: %s sockets connected", connections.length);
-});
+  socket.on("disconnect", function(data) {
+    //disconnect
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("disconnected: %s sockets connected", connections.length);
+  });
 
-io.socket.on("send message", function(data) {
-  console.log(data);
-  io.sockets.emit("new message", {
-    msg: data
+  socket.on("send message", function(data) {
+    console.log(data);
+    io.sockets.emit("new message", {
+      msg: data
+    });
   });
 });
