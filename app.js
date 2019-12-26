66e4af23f8ec035efab434fc2d5bb552ff843490
Kenny <kennymais@gmail.com>
2019-12-26 14:55:21 -0600

    lil 2a app

--- app.js
+++ app.js
@@ -8,24 +8,30 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 console.log("server running");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.sockets.on("connection", function(socket) {
+io.sockets.on("connection", function (socket) {
   connections.push(socket);
   console.log("connected: %s sockets connected", connections.length);
 
-  socket.on("disconnect", function(data) {
+  socket.on("disconnect", function (data) {
     //disconnect
     connections.splice(connections.indexOf(socket), 1);
     console.log("disconnected: %s sockets connected", connections.length);
   });
 
-  socket.on("send message", function(data) {
+  socket.on("send message", function (data) {
     console.log(data);
     io.sockets.emit("new message", {
       msg: data
     });
+  });
+
+
+  socket.on('voice', function (blob) {
+    // can choose to broadcast it to whoever you want
+    socket.broadcast.emit('voice', blob);
   });
 });
