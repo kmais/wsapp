081c03f0ac7c90dd62c26362823d54d0cd64b68c
Kenny <kennymais@gmail.com>
2019-12-26 16:09:51 -0600

    lil 2a app

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ io.sockets.on("connection", function (socket) {
 
     console.log('blob received')
     // can choose to broadcast it to whoever you want
-    io.socket.broadcast.emit('voice', blob);
+    socket.broadcast.emit('voice', blob);
 
   });
 
